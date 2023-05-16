b5c03d0fd36a72f3bea9819936e534f33e6c0183
Grupo2PNT2023 <129802808+Grupo2PNT2023@users.noreply.github.com>
2023-05-15 22:48:44 -0300

    Add files via upload

---
+++ TocoYVoy/TocoYVoy/Jugador.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TocoYVoy
+{
+    internal class Jugador
+    {
+        private String _nombre;
+        private String _apellido;
+        private static int _id;
+        private DateTime _nacimiento;
+        private Puesto _puesto;
+        private String _piernaHabil;
+        private Sexo _sexo;
+
+        public Jugador(string nombre, string apellido, DateTime nacimiento,
+                        Puesto puesto, string piernaHabil, Sexo sexo)
+        {
+            _nombre = nombre;
+            _apellido = apellido;
+            _nacimiento = nacimiento;
+            _puesto = puesto;
+            _piernaHabil = piernaHabil;
+            _sexo = sexo;
+            _id++;
+        }
+
+        public int id { get; set; }
+
+        public Sexo sexo { get; set; }
+    }
+}
---
+++ TocoYVoy/TocoYVoy/Partido.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TocoYVoy
+{
+    public class Partido
+    {
+        private String _lugar;
+        
+        private DateTime _fecha;
+        private TimeOnly _hora;
+        private int _totalJugadores;
+        private List<Jugador> _jugadoresConfirmados = new List<Jugador>();
+        private Tipo _tipo;
+        private Estado _estado;
+
+        public Partido(string lugar, DateTime fecha, TimeOnly hora,
+                         int totalJugadores, Tipo tipo, Estado estado)
+        {
+            _lugar = lugar;
+            _fecha = fecha;
+            _hora = hora;
+            _totalJugadores = totalJugadores;
+            _tipo = tipo;
+            _estado = estado;
+        }
+
+        public int MyProperty { get; set; }
+
+
+
+
+        /*     public Boolean agregarJugador (Jugador jug)
+            {
+                Boolean sePudo = false;
+                _jugadoresConfirmados.Add(jug);
+
+                return sePudo;
+            }
+
+            public Boolean cumpleRequisitos (Jugador jug)
+            {
+                return false;
+            }
+
+            public Boolean buscarJugador(Jugador jug)
+            {
+                Boolean esta = false;
+                Jugador jugadorBuscado = null;
+                int idx = 0;
+                    while(jugadorBuscado == null && !esta) { 
+
+                }
+                return esta;
+            }
+
+            */
+    }
+}
