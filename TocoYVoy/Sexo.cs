24e62c6f11ffbb0d7169765877c270db09ad712a
Grupo2PNT2023 <129802808+Grupo2PNT2023@users.noreply.github.com>
2023-06-26 12:01:08 -0300

    Add files via upload

---
+++ TocoYVoy/Sexo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TocoYVoy
+{
+    public enum Sexo
+    {
+        Hombre,
+        Mujer
+    }
+}
---
+++ TocoYVoy/Tipo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TocoYVoy
+{
+    public enum Tipo
+    {
+        Masculino,
+        Femenino,
+        Mixto
+    }
+}
